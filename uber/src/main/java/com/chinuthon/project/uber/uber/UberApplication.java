ad9abd48aaea6b3d0441f9b692f80d89ae92865b
Chinmay kardpati <karodpatichinmay12@gmail.com>
2026-03-15 01:48:28 +0530

    Spring AI Added

--- uber/src/main/java/com/chinuthon/project/uber/uber/UberApplication.java
+++ uber/src/main/java/com/chinuthon/project/uber/uber/UberApplication.java
@@ -3,7 +3,9 @@ package com.chinuthon.project.uber.uber;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.ai.model.openai.autoconfigure.OpenAiEmbeddingAutoConfiguration.class
+})
 public class UberApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UberApplication.class, args);
